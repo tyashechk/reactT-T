0fe31ad674df56bd5bfc03016b004cba3f819d1e
tyashechk <konarevatoma@gmail.com>
2025-11-09 18:09:51 +0300

    КНОПАКИ

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,21 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "../router/Router";
 
+
 function App() {
+    const createButton = (window as any).createButton;
+    
+    const buttons = [];
+    buttons[0] = createButton("кнопочка");
+    buttons[1] = createButton("кнопчка");
+    buttons[2] = createButton("кнпчк");
+
     return (
         <BrowserRouter>
+            <button>{buttons[0].text}</button>
+            <button>{buttons[1].text}</button>
+            <button>{buttons[2].text}</button>
+            
             <Router />
         </BrowserRouter>
     );
