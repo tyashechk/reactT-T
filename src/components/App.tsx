f9c0eb55f7f17b18fdd7d995ab24202275f4b40d
tyashechk <konarevatoma@gmail.com>
2025-11-10 14:54:33 +0300

    oohuggjjkkyrfmuy

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,19 +3,8 @@ import Router from "../router/Router";
 
 
 function App() {
-    const createButton = (window as any).createButton;
-    
-    const buttons = [];
-    buttons[0] = createButton("кнопочка");
-    buttons[1] = createButton("кнопчка");
-    buttons[2] = createButton("кнпчк");
-
     return (
         <BrowserRouter>
-            <button>{buttons[0].text}</button>
-            <button>{buttons[1].text}</button>
-            <button>{buttons[2].text}</button>
-            
             <Router />
         </BrowserRouter>
     );
